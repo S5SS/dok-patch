de5ebced1141a1cb91b70271ed4fdff5e2d753ba
S5SS <somearbitraryguy@gmail.com>
2018-11-16 00:17:48 +0000

    added map files

--- more-maps/installer-windows.bat
+++ more-maps/installer-windows.bat
@@ -1,18 +1,14 @@
 @echo off
+rem hack to check if run from within the zip
+if not exist "Managed" (
+	echo Please unzip the zip file first!
+	echo Make sure to run the installer you just unzipped instead of the one inside the zip file
+	echo Installation failed!
+	pause
+	exit
+)
+
 rem find steam path
-:ask
-echo Is your SteamLibrary folder in the default path?(Y/N)
-set INPUT=
-set /P INPUT=Type input: %=%
-If /I "%INPUT%"=="y" goto yes 
-If /I "%INPUT%"=="n" goto no
-goto ask
-:no
-@echo you select no
-set /p steam_path="Please paste your full path to SteamLibrary:"
-echo "%steam_path%"
-goto installation
-:yes
 for /F "Tokens=1,2*" %%A In ('Reg Query HKCU\SOFTWARE\Valve\Steam') Do (
     if "%%A" Equ "SteamPath" Set "steam_path=%%C")
 if not defined steam_path (
@@ -23,29 +19,20 @@ if not defined steam_path (
 ) else (
 	echo The Steam folder path is "%steam_path%"
 )
-goto installation
-:installation
-rem hack to check if run from within the zip
-if not exist "Managed" (
-	echo Please unzip the zip file first!
-	echo Make sure to run the installer you just unzipped instead of the one inside the zip file
-	echo Installation failed!
-	pause
-	exit
-)
 
 rem find the dok install location
 set "dok_path=%steam_path%\steamapps\common\Deserts of Kharak"
 if not exist "%dok_path%" (
 	rem TODO: read from %steam_path%\config\config.vdf to get steamapps location
+	:path_loop
 	echo Unable to locate install directory!
-	echo You may have steamapps installed in a different location, please read TROUBLESHOOTING for manual installation instructions
-	echo Installation failed!
-	pause
-	exit
+	echo You may have steamapps installed in a different location
+	set /p dok_path="Please paste your full path to your Deserts of Kharak folder: "
+	
+	if not exist "%dok_path%" goto path_loop
 )
 
-echo The Deserts of Kharak path is "%dok_path%" 
+echo The Deserts of Kharak path is "%dok_path%"
 rem rename managed folder as backup
 echo Creating backup...
 for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined mydate set mydate=%%x
